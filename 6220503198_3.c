8bec26ef6e193a8c6843a5c2e130d1e11100d496
Chavanlak <chavanlak.p@ku.th>
2021-03-11 17:55:57 +0700

    for (int i=1; i<n; i++)

--- 6220503198_3.c
+++ 6220503198_3.c
@@ -1,28 +1,41 @@
 #include<stdio.h>
-    int main()
-{
-    int x,i=1,j,a[3]={2,3,5};
-    int y,z=1;
-    scanf("%d",&x);
-    int ugly[x];
-    ugly[0]=1;
-    for(i=2;z<x;i++)
-    {
-        y=i;
-        for(j=0;j<3;)
-        {
-            if(z%a[j]==0)
-            {
-                y/=a[j];
-            }
-            else
-                j++;
-        }
-        if(y==1)
-        {
-          ugly[z]=i;
-          z++;
-        }
-    }
-    printf("%d",ugly[x-1]);
+int min(int x, int y, int z){
+   if(x<y){
+      if(x < z)
+         return x;
+      else
+         return z;
+   }
+   else{
+      if(y < z)
+         return y;
+      else
+         return z;
+   }
+}
+int getUglyNum(int n){
+   int uglyNum[n];
+   int i2 = 0, i3 = 0, i5 = 0;
+   int next2mul = 2;
+   int next3mul = 3;
+   int next5mul = 5;
+   int next = 1;
+   uglyNum[0] = 1;
+   for (int i=1; i<n; i++){
+      next = min(next2mul, next3mul, next5mul);
+      uglyNum[i] = next;
+      if (next == next2mul){
+         i2++;
+         next2mul = uglyNum[i2]*2;
+      }
+      if (next == next3mul){
+         i3++;
+         next3mul = uglyNum[i3]*3;
+      }
+      if (next == next5mul){
+         i5++;
+         next5mul = uglyNum[i5]*5;
+      }
+   }
+   return next;
 }
