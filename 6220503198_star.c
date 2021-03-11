2134335b326455e1203b8d0f92b42ebe36a5f655
Chavanlak <chavanlak.p@ku.th>
2021-03-11 16:25:34 +0700

    for(j=1;j<=2*n-1;j++)

--- 6220503198_star.c
+++ 6220503198_star.c
@@ -4,12 +4,18 @@ int main()
     int n,i,j,k;
    printf("Enter : ");
    scanf("%d",&n);
-    if(n%2==0)
+    //if(n%2==0)
 
    for(i=1;i<=n;i++)
    {
-       for(j=1;j<=3;j++)
-    printf("*");
+       for(j=1;j<=2*n-1;j++)
+       {
+           if(j>=n-(i-1)&&j<=n+(i-1))
+           {
+                 printf("*");
+           }
+       }
+
         printf("\n");
 
    }
