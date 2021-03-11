dfcc716f6e809681e889fcc6a9164b4889f61183
Chavanlak <chavanlak.p@ku.th>
2021-03-11 16:58:23 +0700

    if(n%2==0)

--- 6220503198_star.c
+++ 6220503198_star.c
@@ -4,21 +4,30 @@ int main()
     int n,i,j,k;
    printf("Enter : ");
    scanf("%d",&n);
-    //if(n%2==0)
+   int x =n/2;
+    if(n%2==0)
+{
+    n++;
+    x =(n-1)/2;
 
+}
    for(i=1;i<=n;i++)
    {
        for(j=1;j<=2*n-1;j++)
        {
            if(j>=n-(i-1)&&j<=n+(i-1))
            {
-                 printf("*");
+                 printf("-");
+           }
+           else
+           {
+               printf("*");
            }
        }
 
         printf("\n");
-
    }
+
 
 
 }
